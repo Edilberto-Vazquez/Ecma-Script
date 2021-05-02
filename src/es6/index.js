6bb2251e4a1fa1cd782ae9e4d0c49c0e3369057a
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-01 23:25:57 -0500

    Arrow functions

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,4 +1,4 @@
-// Propiedad de objetos mejorada
+// ---Propiedad de objetos mejorada---
 let nombre = 'Edi'
 let age = 32
 
@@ -7,4 +7,32 @@ obj = {nombre: nombre, age: age}
 
 // ES6
 obj2 = {nombre, age}
+console.log(obj)
 console.log(obj2)
+
+// ---Arrow function---
+const nombres = [
+    {nombre: 'Edi', age: 32},
+    {nombre: 'Jose', age: 27}
+]
+
+// Vieja forma function map
+let listOfNames = nombres.map(function (item) {
+    console.log(item.nombre)
+})
+
+// ES6 arrow function map
+let listOfNames2 = nombres.map(item => console.log(item.nombre))
+
+// ES6 arrow function forma 1
+const listOfNames3 = (nombre, age) => {
+    ...
+}
+
+// ES6 arrow function forma 2
+const listOfNames4 = nombre => {
+    ...
+}
+
+// ES6 arrow function forma 3
+const square = num => num * num
