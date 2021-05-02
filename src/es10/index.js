3bfd4555db970cab97bd6bcf020d3e24dd634a58
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-02 01:53:41 -0500

    trimStart y trimEnd

--- src/es10/index.js
+++ src/es10/index.js
@@ -7,3 +7,13 @@ console.log(array.flat(2))
 let array = [1,2,3,4,5]
 
 console.log(array.flatMap(value => [value, value * 2]))
+
+// trimStart elimina los espacios en blanco al principio del string
+let hello = '        hello world'
+console.log(hello)
+console.log(hello.trimStart())
+
+// trimEnd elimina los espacios en blanco al final de string
+let hello = 'hello world      '
+console.log(hello)
+console.log(hello.trimEnd())
