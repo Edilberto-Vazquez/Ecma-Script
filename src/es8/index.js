7bf942b94cc8a975a659d2cd51e8136961dfcfd8
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-02 01:15:00 -0500

    Asysn y Await

---
+++ src/es8/index.js
@@ -0,0 +1,26 @@
+// Async y Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+            ? setTimeout(() => resolve('Hello World'), 300)
+            : reject(new Error('Test error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld()
+    console.log(hello)
+}
+
+helloAsync()
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld()
+        console.log(hello)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+anotherFunction()
