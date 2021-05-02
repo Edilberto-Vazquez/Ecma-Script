e5d24f03dad53a95eadbbad94509f573cf52d257
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-01 21:49:01 -0500

    Desestructuracion de objetos/elementos

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,4 +1,4 @@
-// Multilinea
+// ---Multilinea---
 // version vieja
 let lorem = "frase epica a separar\n"
 + "otra frase epica que se necesita."
@@ -10,3 +10,17 @@ ahora es otra frase epica
 
 console.log(lorem)
 console.log(lorem2)
+
+// ---Destructuracion de elementos---
+let person = {
+    'name': 'oscar',
+    'age' : 32,
+    'country' : 'MX'
+}
+
+// version vieja
+console.log(person.name, person.age);
+
+// ES6
+let { name, age} = person
+console.log(name, age)
