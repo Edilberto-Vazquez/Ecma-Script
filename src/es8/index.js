a5cc5ff57d26a92ba9912f7e865ec84769a38a6b
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-02 00:46:29 -0500

    Object values

--- src/es8/index.js
+++ src/es8/index.js
@@ -8,3 +8,14 @@ const data = {
 const entries = Object.entries(data);
 console.log(entries)
 console.log(entries.length)
+
+// Object values convierte los valores del objeto a arreglo
+const data = {
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana'
+}
+
+const values = Object.values(data)
+console.log(values)
+console.log(values.length)
