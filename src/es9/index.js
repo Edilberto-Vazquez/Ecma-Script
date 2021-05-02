10539399bbff6e99b9f2560e80cd895f41c73ef7
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-02 01:40:10 -0500

    regex

--- src/es9/index.js
+++ src/es9/index.js
@@ -34,3 +34,17 @@ const helloWorld = () => {
             : reject(new Error('Test Error'))
     })
 }
+
+// Regex
+helloWorld()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizo'))
+
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+const match = regexData.exec('2020-04-20')
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(year, month, day)
