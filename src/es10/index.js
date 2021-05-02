948aef0812b287e751966eb06663cfa5a48e140d
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-02 01:48:26 -0500

    flatMap metod

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,4 +1,9 @@
-// flat metod
+// flat metod aplana un arreglo
 let array = [1,2,3, [1,2,3, [1,2,3]]]
 
 console.log(array.flat(2))
+
+// flatMap metod
+let array = [1,2,3,4,5]
+
+console.log(array.flatMap(value => [value, value * 2]))
