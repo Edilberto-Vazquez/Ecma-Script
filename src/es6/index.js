d658c8d5a546cffc364e885169797413eebf0aaf
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-02 00:25:48 -0500

    Generators

--- src/es6/index.js
+++ src/es6/index.js
@@ -22,3 +22,18 @@ console.log(calc.sum(2,2))
 import hello from './module.js'
 
 hello()
+
+// Generators
+function* helloWorld() {
+    if (true) {
+        yield 'Hello, '
+    }
+    if (true) {
+        yield 'World'
+    }
+}
+
+const generatorHello = helloWorld()
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
